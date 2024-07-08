e8dda3c54c13686ab2b9fec01f4fd63372ce67fe
Praval <p.singhx26@gmail.com>
2024-07-08 21:33:07 +0530

    setting home template

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class FormDatabase(models.Model):
+    firstName = models.CharField(max_length=80)
+    lastName = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"

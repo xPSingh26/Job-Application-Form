7dbc09e9bcb0ead7b597716e0e821f816e820173
Praval <p.singhx26@gmail.com>
2024-07-10 21:01:17 +0530

    storing user input

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 def index(request):
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            firstName = form.cleaned_data['first_name']
+            lastName = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            print(firstName)
+
     return render(request, 'index.html')
